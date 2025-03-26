c290db9081b003c5f7d57ff961134565da1c1389
Alucsky <samuelmolinari8@gmail.com>
2025-03-26 14:11:30 -0300

    theme of app

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,29 +1,31 @@
 export default {
   COLORS: {
     WHITE: '#FFFFFF',
-
-    GREEN_700: '#00875F',
-    GREEN_500: '#00B37E',
-
-    RED: '#F75A68',
-    RED_DARK: '#AA2834',
-
-    GRAY_700: '#121214',
-    GRAY_600: '#202024',
-    GRAY_500: '#29292E',
-    GRAY_400: '#323238',
-    GRAY_300: '#7C7C8A',
-    GRAY_200: '#C4C4CC',
-    GRAY_100: '#E1E1E6',
+    RED_DARK: '#BF3B44',
+    RED_MID: '#F3BABD',
+    RED_LIGHT: '#F4E6E7',
+    GREEN_DARK: '#639339',
+    GREEN_MID: '#CBEAB4',
+    GREEN_LIGHT: '#E5F0DB',
+    GRAY_1: '#1B1D1E',
+    GRAY_2: '#333638',
+    GRAY_3: '#5C6265',
+    GRAY_4: '#99BBBC',
+    GRAY_5: '#DDDEDF',
+    GRAY_6: '#EFFDFD',
+    GRAY_7: '#FAFAFA'
   },
   FONT_FAMILY: {
-    REGULAR: 'Roboto_400Regular',
-    BOLD: 'Roboto_700Bold',
+    REGULAR: 'Nunito Sans_400Regular',
+    BOLD: 'Nunito Sans_700Bold',
   },
   FONT_SIZE: {
+    XS: 12,
     SM: 14,
     MD: 16,
     LG: 18,
     XL: 24,
+    XXL: 32
   },
+  LINE_HEIGHT: 1.3
 }
